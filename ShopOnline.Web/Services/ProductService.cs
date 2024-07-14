f595ab20676b709bfba1681491f4819e65df0b89
jackgusler <guslerj1@newpaltz.edu>
2024-07-14 14:21:35 -0400

    specific product data displayed

--- ShopOnline.Web/Services/ProductService.cs
+++ ShopOnline.Web/Services/ProductService.cs
@@ -13,19 +13,60 @@ namespace ShopOnline.Web.Services
             this.httpClient = httpClient;
         }
 
-        public async Task<IEnumerable<ProductDto>> GetItems()
+		public async Task<ProductDto> GetItem(int id)
+		{
+			try
+			{
+				var response = await httpClient.GetAsync($"api/Product/{id}");
+
+				if (response.IsSuccessStatusCode)
+				{
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        return default(ProductDto);
+                    }
+
+                    return await response.Content.ReadFromJsonAsync<ProductDto>();
+				}
+                else
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception(message);
+                }
+			}
+			catch (Exception)
+			{
+				// Log the exception
+				throw;
+			}
+		}
+
+		public async Task<IEnumerable<ProductDto>> GetItems()
         {
             try
             {
-                var products = await this.httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>("api/Product");
+                var response = await this.httpClient.GetAsync("api/Product");
 
-                return products;
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        return Enumerable.Empty<ProductDto>();
+                    }
+
+                    return await response.Content.ReadFromJsonAsync<IEnumerable<ProductDto>>();
+                }
+                else
+                {
+                    var message = await response.Content.ReadAsStringAsync();
+                    throw new Exception(message);
+                }
             }
             catch (Exception)
-            {
-                // Log the exception
-                throw;
-            }
+			{
+				// Log the exception
+				throw;
+			}
         }
     }
 }
