3bae0bfff1b0a9822f398d21f4d0783d95f0b06e
jackgusler <guslerj1@newpaltz.edu>
2024-07-14 00:16:15 -0400

    created database

--- ShopOnline.Api/Data/ShopOnlineDbContext.cs
+++ ShopOnline.Api/Data/ShopOnlineDbContext.cs
@@ -310,5 +310,11 @@ namespace ShopOnline.Api.Data
                 Name = "Shoes"
             });
         }
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
