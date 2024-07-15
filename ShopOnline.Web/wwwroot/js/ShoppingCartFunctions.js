70bc2686e0c5c59f23365d4d618c7fd3cb46c7aa
jackgusler <guslerj1@newpaltz.edu>
2024-07-15 17:20:46 -0400

    shopping cart update quantity

---
+++ ShopOnline.Web/wwwroot/js/ShoppingCartFunctions.js
@@ -0,0 +1,5 @@
+﻿function MakeUpdateQtyButtonVisible(id, visible) {
+    const updateQtyButton = document.querySelector("button[data-itemId='" + id + "']")
+
+    visible ? updateQtyButton.style.display = "inline-block" : updateQtyButton.style.display = "none"
+}
